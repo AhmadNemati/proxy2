484aefb0beb09130f66120d83642615c0355808b
Ahmad Nemati <nematiprog@gmail.com>
2018-07-11 11:45:17 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ProxyChain = require('proxy-chain');
  
-const server = new ProxyChain.Server({ port: 8000 });
+const server = new ProxyChain.Server({ port: 3128 });
  
 server.listen(() => {
     console.log(`Proxy server is listening on port ${8000}`);
